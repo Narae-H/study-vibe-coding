991c45629aafc652a7167d3608f535c14ad0fd6f
Narae-H <naraehyeon@gmail.com>
2025-10-15 15:44:14 +1100

    feat: 감정(emotion) enum 상수 구현
    - 5가지 감정 타입(행복해요, 슬퍼요, 화나요, 놀랐어요, 기타) 정의
    - 감정별 라벨, 이미지(medium/small), 색상 데이터 구현
    - EmotionData interface로 타입 안정성 확보
    - Record<EmotionType, EmotionData>로 타입 체크 강화

--- 1. Code Examples/mood-diary/src/commons/constants/enum.ts
+++ 1. Code Examples/mood-diary/src/commons/constants/enum.ts
@@ -22,10 +22,8 @@ export enum EmotionType {
  */
 interface EmotionData {
   label: string;
-  image: {
-    medium: string;
-    small: string;
-  };
+  imageMedium: string;
+  imageSmall: string;
   color: string;
 }
 
@@ -35,78 +33,32 @@ interface EmotionData {
 export const EMOTION_DATA: Record<EmotionType, EmotionData> = {
   [EmotionType.HAPPY]: {
     label: '행복해요',
-    image: {
-      medium: '/images/emotion-happy-m.png',
-      small: '/images/emotion-happy-s.png',
-    },
+    imageMedium: '/images/emotion-happy-m.png',
+    imageSmall: '/images/emotion-happy-s.png',
     color: red[60],
   },
   [EmotionType.SAD]: {
     label: '슬퍼요',
-    image: {
-      medium: '/images/emotion-sad-m.png',
-      small: '/images/emotion-sad-s.png',
-    },
+    imageMedium: '/images/emotion-sad-m.png',
+    imageSmall: '/images/emotion-sad-s.png',
     color: blue[60],
   },
   [EmotionType.ANGRY]: {
     label: '화나요',
-    image: {
-      medium: '/images/emotion-angry-m.png',
-      small: '/images/emotion-angry-s.png',
-    },
+    imageMedium: '/images/emotion-angry-m.png',
+    imageSmall: '/images/emotion-angry-s.png',
     color: gray[60],
   },
   [EmotionType.SURPRISE]: {
     label: '놀랐어요',
-    image: {
-      medium: '/images/emotion-surprise-m.png',
-      small: '/images/emotion-surprise-s.png',
-    },
+    imageMedium: '/images/emotion-surprise-m.png',
+    imageSmall: '/images/emotion-surprise-s.png',
     color: yellow[60],
   },
   [EmotionType.ETC]: {
     label: '기타',
-    image: {
-      medium: '/images/emotion-etc-m.png',
-      small: '/images/emotion-etc-s.png',
-    },
+    imageMedium: '/images/emotion-etc-m.png',
+    imageSmall: '/images/emotion-etc-s.png',
     color: green[60],
   },
 } as const;
-
-/**
- * 감정 타입 배열 (순서 보장)
- */
-export const EMOTION_LIST = [
-  EmotionType.HAPPY,
-  EmotionType.SAD,
-  EmotionType.ANGRY,
-  EmotionType.SURPRISE,
-  EmotionType.ETC,
-] as const;
-
-/**
- * 감정 타입별 라벨 가져오기
- */
-export const getEmotionLabel = (emotion: EmotionType): string => {
-  return EMOTION_DATA[emotion].label;
-};
-
-/**
- * 감정 타입별 이미지 경로 가져오기
- */
-export const getEmotionImage = (
-  emotion: EmotionType,
-  size: 'medium' | 'small' = 'medium'
-): string => {
-  return EMOTION_DATA[emotion].image[size];
-};
-
-/**
- * 감정 타입별 색상 가져오기
- */
-export const getEmotionColor = (emotion: EmotionType): string => {
-  return EMOTION_DATA[emotion].color;
-};
-
