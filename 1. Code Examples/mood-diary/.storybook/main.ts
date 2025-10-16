9a6fb1029dffeca6c20c9862cd52cace31d66aa1
NaraeH <79120733+Narae-H@users.noreply.github.com>
2025-10-16 23:15:35 +1100

    feat: storybook 의존성 에러 해결

--- 1. Code Examples/mood-diary/.storybook/main.ts
+++ 1. Code Examples/mood-diary/.storybook/main.ts
@@ -1,56 +1,21 @@
-import type { StorybookConfig } from '@storybook/react-webpack5';
-import path from 'path';
+import type { StorybookConfig } from '@storybook/experimental-nextjs-vite';
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: [
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/addon-links',
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
-  framework: {
-    name: '@storybook/react-webpack5',
-    options: {},
+  "addons": [
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@chromatic-com/storybook"
+  ],
+  "framework": {
+    "name": "@storybook/experimental-nextjs-vite",
+    "options": {}
   },
-  staticDirs: ['../public'],
-  typescript: {
-    check: false,
-    reactDocgen: 'react-docgen-typescript',
-    reactDocgenTypescriptOptions: {
-      shouldExtractLiteralValuesFromEnum: true,
-      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
-    },
-  },
-  webpackFinal: async (config) => {
-    // CSS 모듈 지원
-    config.module = config.module || {};
-    config.module.rules = config.module.rules || [];
-    
-    // CSS 모듈 규칙 추가
-    config.module.rules.push({
-      test: /\.module\.css$/,
-      use: [
-        'style-loader',
-        {
-          loader: 'css-loader',
-          options: {
-            modules: {
-              localIdentName: '[name]__[local]--[hash:base64:5]',
-            },
-          },
-        },
-      ],
-    });
-
-    // 절대 경로 설정
-    config.resolve = config.resolve || {};
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.resolve(__dirname, '../src'),
-    };
-
-    return config;
-  },
+  "staticDirs": [
+    "../public"
+  ]
 };
-
 export default config;
